8664d4ac0dfe5a92d3eb9b35d3358c055ff1402e
Anthony-Perriolat <pro.aperriolat@gmail.com>
2021-04-22 16:41:26 +0200

    finction final #6

--- drive.js
+++ drive.js
@@ -7,12 +7,12 @@ const { error } = require('console');
 const http = require('http');
 
 //Chemin du drive en local
-const pathDrive = os.tmpdir()+path.sep
-if (!fs.existsSync(pathDrive+'drive')) {
-fs.promises.mkdir(pathDrive+'drive')
-.then(() => console.log('repertoire crée'))
-.catch((err) => console.log(err))
-console.log(pathDrive)
+const pathDrive = os.tmpdir() + path.sep
+if (!fs.existsSync(pathDrive + 'drive')) {
+    fs.promises.mkdir(pathDrive + 'drive')
+        .then(() => console.log('repertoire crée'))
+        .catch((err) => console.log(err))
+    console.log(pathDrive)
 }
 else {
     console.log('Le dossier à déjà était crée')
@@ -21,46 +21,57 @@ else {
 // Retourne une liste contenant les dossiers et fichiers à la racine du “drive” sous forme de array
 
 function listDrive(target) {
-    let arrayDrive = fs.promises.readdir(pathDrive+target, {withFileTypes:true})
+    let arrayDrive = fs.promises.readdir(pathDrive + target, { withFileTypes: true })
     const transformPromise = arrayDrive.then((result) => {
         const tab = [];
-        result.forEach(function (element){
+        result.forEach(function (element) {
             tab.push({
-                name : element.name,
-                isFolder : element.isDirectory()                
+                name: element.name,
+                isFolder: element.isDirectory()
             })
-
-    })
-    return tab;
+        })
+        return tab;
     })
 
     return transformPromise;
-} 
+}
 function isFolder(target) {
-    let isFolder = fs.promises.stat(pathDrive+target) 
-    .then((stats) => stats.isDirectory())
+    let isFolder = fs.promises.stat(pathDrive + target)
+        .then((stats) => stats.isDirectory())
     return isFolder;
 }
 function readFile(target) {
-    const readingTarget = fs.promises.readFile(pathDrive+target, {encoding : 'UTF-8'})
+    const readingTarget = fs.promises.readFile(pathDrive + target, { encoding: 'UTF-8' })
     return readingTarget
 }
-function addFolder(name='') {
-    const add = fs.promises.mkdir(pathDrive+'drive'+path.sep+name)
-    .then(() => console.log('le dossier '+ name +' à été crée'))
-    .catch((err => console.log(err)))
+function addFolder(name = '') {
+    const add = fs.promises.mkdir(pathDrive + 'drive' + path.sep + name)
+        .then(() => console.log('le dossier ' + name + ' à été crée'))
+        .catch((err => console.log(err)))
     return add
-
 }
 function deleteElement(name) {
-    const del = fs.promises.rm(pathDrive+'drive'+path.sep+name ,{recursive: true, force: true})
+    const del = fs.promises.rm(pathDrive + 'drive' + path.sep + name, { recursive: true, force: true })
     return del
 }
+/* function isValid(name) {
+    return /^[a-zA-Z0-9_()\. ]*$test(name)
+}
+ */
+// Upload 
+function upload(tmp, name, uuid, folder='') {
+    const up = fs.promises.copyFile(tmp, pathDrive + 'drive' + path.sep + folder + name)
+    .then(fs.promises.rm(os.tmpdir()+path.sep+'express-busboy/'+uuid, { recursive: true, force: true }))
+    return up
+}
 
-module.exports = {listDrive:listDrive,
-    isFolder:isFolder,
-    readFile:readFile,
-    addFolder:addFolder,
-    deleteElement:deleteElement
+module.exports = {
+    listDrive: listDrive,
+    isFolder: isFolder,
+    readFile: readFile,
+    addFolder: addFolder,
+    deleteElement: deleteElement,
+    upload: upload,
+    pathDrive: pathDrive
 }
 
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+const bb = require('express-busboy');
 const express = require('express')
-const { openFolder } = require('./drive')
 const drive = require('./drive')
+const path = require('path');
+const { addFolder } = require('./drive');
 const app = express()
 const port = 3000
+const options = {
+  upload: true,
+/*   path: drive.pathDrive+path.sep+'drive',
+ */  allowedPath: /./,
+}
+bb.extend(app, options);
 
 app.use(express.static('frontend'))
 
@@ -11,22 +19,23 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
+
+
 // indexation drive 
 app.get('/api/drive', (req, res) => {
   drive.listDrive('drive').then((content) => res.json(content))
 })
 // Selection dossier ou lecture 
 app.get('/api/drive/:name', (req, res) => {
-  drive.isFolder('drive/'+req.params.name).then((content) => {
+  drive.isFolder('drive/' + req.params.name).then((content) => {
     if (content) {
-      drive.listDrive('drive/'+req.params.name).then((content) => res.json(content))
+      drive.listDrive('drive/' + req.params.name).then((content) => res.json(content))
     }
-    else
-    {
-      drive.readFile('drive/'+req.params.name).then((content) => res.send(content))
+    else {
+      drive.readFile('drive/' + req.params.name).then((content) => res.send(content))
     }
   })
-  .catch( () => res.status(404).send('erreur : 404'))
+    .catch(() => res.status(404).send('erreur : 404'))
 })
 // ajout dossier
 app.post('/api/drive', (req, res) => {
@@ -36,15 +45,16 @@ app.post('/api/drive', (req, res) => {
 })
 // ajout dossier dans un dossier
 app.post('/api/drive/:folder', (req, res) => {
-  drive.addFolder(req.params.folder+'/'+req.query.name).then(() => {
+  drive.addFolder(req.params.folder + path.sep + req.query.name)
+  .then(() => {
     res.status(201).end()
   }).catch(() => {
     if (!req.params.folder)
-    res.status(404).end()
+      res.status(404).end()
   })
 })
 // Suppression d'un dossier
-app.delete('/api/drive/:name',(req, res) => {
+app.delete('/api/drive/:name', (req, res) => {
   drive.deleteElement(req.params.name).then(() => {
     res.status(201).end()
   }).catch(() => {
@@ -52,10 +62,26 @@ app.delete('/api/drive/:name',(req, res) => {
   })
 })
 // Suppression d'un dossier dans un dossier
-app.delete('/api/drive/:folder/:name',(req, res) => {
-  drive.deleteElement(req.params.folder+'/'+req.params.name).then(() => {
+app.delete('/api/drive/:folder/:name', (req, res) => {
+  drive.deleteElement(req.params.folder + '/' + req.params.name).then(() => {
     res.status(201).end()
   }).catch(() => {
     res.status(400).end()
   })
+})
+// Upload fichier 
+app.put('/api/drive', (req, res) => {
+  const uuid = req.files.file.uuid
+  const pathTmp = req.files.file.file
+  const name = path.basename(pathTmp)
+  drive.upload(pathTmp, name, uuid)
+  .then(res.status(200).send(console.log('suppression confirmé')))
+})
+// Upload fichier dans un dossier
+app.put('/api/drive/:folder', (req, res) => {
+  const uuid = req.files.file.uuid
+  const pathTmp = req.files.file.file
+  const name = path.basename(pathTmp)
+  drive.upload(pathTmp, name, uuid,req.params.folder+path.sep)
+  .then(res.status(200).send(console.log('suppression confirmé et création effectué')))
 })
